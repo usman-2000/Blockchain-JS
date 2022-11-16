ed4088a6020d06772a924a3e226633b80f5fbbbb
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-11-16 12:23:05 +0500

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,22 @@
+// synchronous [Solidity] --- one track, One operation at a time
+// asynchronous [Javascript] --- runs code parallel way, second operation can run while one operation is being processed
+
+// example : Cooking
+
+// < Promise >   --  wo functions jahan wait krana ho wo promise functions hoty
+// Three types k hoty promises:
+// 1: Pending
+// 2: fulfilled
+// 3: Rejected
+
+// Another example of these are in cooking .js
+
+
+
+function main() {
+  console.log("Hello");
+
+  let variable = 5;
+  console.log(variable);
+}
+main()
